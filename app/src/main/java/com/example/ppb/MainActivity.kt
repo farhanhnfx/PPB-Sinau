f183ba090bef973b379a8db62af90cacca517136
farhanhnfx <farhanhanif@mail.ugm.ac.id>
2023-11-22 10:33:12 +0700

    pertemuan 13 - latihan firebase firestore

--- app/src/main/java/com/example/ppb/MainActivity.kt
+++ app/src/main/java/com/example/ppb/MainActivity.kt
@@ -2,91 +2,116 @@ package com.example.ppb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.util.Log
+import android.widget.ArrayAdapter
+import androidx.lifecycle.MutableLiveData
 import com.example.ppb.databinding.ActivityMainBinding
-import java.text.NumberFormat
-import java.util.Locale
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
-    val binding by lazy {
+    private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-    lateinit var mNotesDao: NoteDao
-    lateinit var executorService: ExecutorService
+    private val firestore = FirebaseFirestore.getInstance()
+    private val budgetCollectionRef = firestore.collection("budgets")
+//    private val firestore by lazy {
+//        FirebaseFirestore.getInstance()
+//    }
+//    private val budgetCollectionRef by lazy {
+//        firestore.collection("budgets")
+//    }
+    private var updateId = ""
+    private val budgetListLiveData: MutableLiveData<List<Budget>> by lazy {
+        MutableLiveData<List<Budget>>()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+//        FirebaseApp.initializeApp(this)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        observeBudgets()
+        getAllBudgets()
         with(binding) {
-            btnAdd.setOnClickListener{
-                showPopupDialog()
+            btnAdd.setOnClickListener {
+                val nominal = editNominal.text.toString()
+                val desc = editDesc.text.toString()
+                val date = editDate.text.toString()
+                val newBudget = Budget(nominal = nominal, description = desc, date = date)
+                addBudget(newBudget)
             }
-            rvContent.addOnScrollListener(object: RecyclerView.OnScrollListener() {
-                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                    super.onScrolled(recyclerView, dx, dy)
-                    if (dy > 0 || dy < 0) {
-                        btnAdd.visibility = View.GONE
-                    }
-                }
-                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                    super.onScrollStateChanged(recyclerView, newState)
-                    if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                        btnAdd.visibility = View.VISIBLE
-                    }
-                }
-            })
-        }
-
-        executorService = Executors.newSingleThreadExecutor()
-        val db = NoteRoomDB.getDatabase(this)
-        mNotesDao = db!!.noteDao()!!
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        getAllNotes()
-    }
-
-    private fun getAllNotes() {
-        mNotesDao.allNotes.observe(this) {
-            notes ->
-            val listAdapter = NoteAdapter(notes) {
-                selectedNote ->
-                showPopupDialog(selectedNote)
+            btnUpdate.setOnClickListener {
+                val nominal = editNominal.text.toString()
+                val desc = editDesc.text.toString()
+                val date = editDate.text.toString()
+                val updatedBudget = Budget(nominal = nominal, description = desc, date = date)
+                updateBudget(updatedBudget)
+                updateId = ""
+                setEmptyFields()
             }
-            binding.rvContent.apply {
-                adapter = listAdapter
-                layoutManager = LinearLayoutManager(this@MainActivity)
-            }
-            val total = calculateTotal(notes)
-            binding.txtTotal.text = NumberFormat.getCurrencyInstance(Locale("id", "ID")).format(total)
-        }
-    }
-
-    private fun showPopupDialog(note: Note? = null) {
-        val notePopup = NotePopupDialog(this, note)
-        notePopup.show(supportFragmentManager, "NotePopup")
-    }
-
-    private fun calculateTotal(notes: List<Note>): Int {
-        val plus = ArrayList<Int>()
-        val minus = ArrayList<Int>()
-        for (note in notes) {
-            if (note.category == "Pemasukan") {
-                plus.add(note.amount)
-            }
-            else {
-                minus.add(note.amount)
+            listView.setOnItemClickListener { adapterView, view, i, l ->
+                val item = adapterView.adapter.getItem(i) as Budget
+                updateId = item.id
+                editNominal.setText(item.nominal)
+                editDesc.setText(item.description)
+                editDate.setText(item.date)
             }
         }
-        val sumA = plus.sum()
-        val sumB = minus.sum()
-        return sumA - sumB
+    }
+
+    private fun getAllBudgets() {
+        observeBudgetChanges();
+    }
+
+    private fun observeBudgetChanges() {
+        budgetCollectionRef.addSnapshotListener { value, error ->
+            if (error != null) {
+                Log.d("MainActivityLOG", "Error listening for budget changes: $error")
+            }
+            val budgets = value?.toObjects(Budget::class.java)
+            if (budgets != null) {
+                budgetListLiveData.postValue(budgets)
+            }
+        }
+    }
+    private fun observeBudgets() {
+        budgetListLiveData.observe(this) {
+            budgets ->
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, budgets.toMutableList())
+            binding.listView.adapter = adapter
+        }
+    }
+    private fun addBudget(budget: Budget) {
+        budgetCollectionRef.add(budget).addOnSuccessListener {
+            documentReference ->
+            val createdBudgetId = documentReference.id
+            budget.id = createdBudgetId
+            documentReference.set(budget).addOnFailureListener {
+                Log.d("MainActivityLOG", "Error updating budget id: $it")
+            }
+        }.addOnFailureListener {
+            Log.d("MainActivityLOG", "Error adding budget id: $it")
+        }
+    }
+    private fun updateBudget(budget: Budget) {
+        budget.id = updateId
+        budgetCollectionRef.document(updateId).set(budget).addOnFailureListener {
+            Log.d("MainActivityLOG", "Error updating budget: $it")
+        }
+    }
+    private fun deleteBudget(budget: Budget) {
+        if (budget.id.isEmpty()) {
+            Log.d("MainActivityLOG", "Error deleting budget: empty id")
+        }
+        budgetCollectionRef.document(budget.id).delete().addOnFailureListener {
+            Log.d("MainActivityLOG", "Error deleting budget: $it")
+        }
+    }
+    private fun setEmptyFields() {
+        with(binding) {
+            editNominal.setText("")
+            editDesc.setText("")
+            editDate.setText("")
+        }
     }
 }
